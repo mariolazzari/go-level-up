7c765ef24f5cbe9721adce2eea43351f4080a0ea
Mario Lazzari <mario.lazzari@gmail.com>
2025-11-23 15:23:34 +0100

    Section 3 lesson 33

---
+++ 1_go/03_raffle/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"time"
+)
+
+const path = "entries.json"
+
+// raffleEntry is the struct we unmarshal raffle entries into
+type raffleEntry struct {
+	ID   int    `json:"id"`
+	Name string `json:"name`
+}
+
+// importData reads the raffle entries from file and creates the entries slice.
+func importData() []raffleEntry {
+	panic("NOT IMPLEMENTED")
+}
+
+// getWinner returns a random winner from a slice of raffle entries.
+func getWinner(entries []raffleEntry) raffleEntry {
+	rand.Seed(time.Now().Unix())
+	wi := rand.Intn(len(entries))
+	return entries[wi]
+}
+
+func main() {
+	entries := importData()
+	log.Println("And... the raffle winning entry is...")
+	winner := getWinner(entries)
+	time.Sleep(500 * time.Millisecond)
+	log.Println(winner)
+}
